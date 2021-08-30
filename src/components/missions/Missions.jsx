214a0562314b4451faf726bf10ae9b022207ee87
Usman Suleiman <usmansbk@gmail.com>
2021-08-30 11:20:49 +0100

    Fetch missions

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,3 +1,20 @@
-const Missions = () => <h1>Missions</h1>;
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMissions } from '../../redux/missions/missions';
+
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+
+  useEffect(() => {
+    if (!missions.length) {
+      dispatch(fetchMissions);
+    }
+  }, []);
+
+  return (
+    <h1>Missions</h1>
+  );
+};
 
 export default Missions;
