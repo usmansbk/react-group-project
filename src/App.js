60348a3ff98a8189b97501c9b33daf10ff1273c2
Usman Suleiman <usmansbk@gmail.com>
2021-08-30 10:31:09 +0100

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 import {
   HashRouter as Router, NavLink, Switch, Route,
 } from 'react-router-dom';
@@ -6,6 +9,7 @@ import Missions from './components/missions/Missions';
 import Rockets from './components/rockets/Rockets';
 import Profile from './components/profile/Profile';
 import store from './redux/configStore';
+import logo from './logo.svg';
 import './App.css';
 
 const routes = [
@@ -27,19 +31,34 @@ const routes = [
 ];
 
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        {routes.map(({ name, path }) => (
-          <li key={path}><NavLink activeClassName="App-active-link" exact to={path}>{name}</NavLink></li>))}
-      </ul>
-    </nav>
-  </header>
+  <Navbar sticky="top" bg="white">
+    <Container className="py-2 border-bottom">
+      <Navbar.Brand className="h1">
+        <img src={logo} className="App-logo" alt="logo" />
+        {' '}
+        Space Traveler&apos;s Hub
+      </Navbar.Brand>
+      <Nav>
+        {(routes.map(({ path, name }) => (
+          <Nav.Link key={path}>
+            <NavLink
+              className="App-link"
+              activeClassName="App-active-link"
+              exact
+              to={path}
+            >
+              {name}
+            </NavLink>
+          </Nav.Link>
+        )))}
+      </Nav>
+    </Container>
+  </Navbar>
 );
 
 const App = () => (
   <Provider store={store}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Header />
       <Switch>
         {routes.map(({ path, component }) => (
