9414cf46501d0c9b7ab11c6af79a548219c29f83
Hiro Mataba <aganzemataba@gmail.com>
2021-08-30 12:02:17 +0200

    Update rockects

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,8 +1,5 @@
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
+const BASE_URL = 'https://api.spacexdata.com/v3/rockets';
+const LOAD_ROCKETS = 'rockets/load';
+const BOOK_ROCKET = 'rockets/book';
+const CANCEL_BOOKING = 'rockets/cancel-booking';
 
-export default reducer;
