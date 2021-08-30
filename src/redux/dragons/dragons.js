9be2e329b4a6bc569040bf5d1f24c8c981b9f2bc
olawale-o <omoogunolawale@gmail.com>
2021-08-30 12:26:17 +0100

    Create loadDragons and fetchDragons functions in dragons.js file

--- src/redux/dragons/dragons.js
+++ src/redux/dragons/dragons.js
@@ -1,5 +1,28 @@
+const BASE_URL = 'https://api.spacexdata.com/v3/dragons';
+const LOAD_DRAGONS = 'dragons/load';
+
+const loadDragons = (payload) => ({
+  type: LOAD_DRAGONS,
+  payload,
+});
+
+export const fetchDragons = async (dispatch) => {
+  const response = await fetch(BASE_URL);
+  const dragons = await response.json();
+
+  dispatch(loadDragons(dragons.map((dragon) => ({
+    id: dragon.id,
+    name: dragon.name,
+    type: dragon.type,
+    description: dragon.description,
+    images: dragon.flickr_images,
+  }))));
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
+    case LOAD_DRAGONS:
+      return action.payload;
     default:
       return state;
   }
