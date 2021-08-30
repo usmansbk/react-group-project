691d9c4f3cb518d42e0e80fe931a4aa4600e1ae5
Hiro Mataba <aganzemataba@gmail.com>
2021-08-30 12:09:33 +0200

    Add import to Rockets Component

--- src/components/rockets/Rockets.jsx
+++ src/components/rockets/Rockets.jsx
@@ -1,3 +1,9 @@
-const Rockets = () => <h1>Rockets</h1>;
-
-export default Rockets;
+import { useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import { useSelector, useDispatch } from 'react-redux';
+import { bookRocket, cancelBooking, fetchRockets } from '../../redux/rockets/rockets';
